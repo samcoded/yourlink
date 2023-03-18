a24ee939cf6cd9c78e530781493306e32b672c35
Samuel Anozie <samuelanozie10@yahoo.com>
2023-03-18 21:57:22 +0100

    User controllers

---
+++ src/controllers/user.ts
@@ -0,0 +1,51 @@
+// user login and registration controller
+
+import { Request, Response } from 'express';
+import User from '../models/user';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { hashPassword, comparePassword } from '../utils';
+
+dotenv.config();
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const user = new User(req.body);
+
+        user.password = await hashPassword(user.password);
+        await user.save();
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET!);
+        return res.json({ token, user });
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Email is taken',
+        });
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                error: 'Email or password is incorrect',
+            });
+        }
+
+        const isMatch = await comparePassword(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                error: 'Email or password is incorrect',
+            });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!);
+        return res.json({ token, user });
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Email or password is incorrect',
+        });
+    }
+};
