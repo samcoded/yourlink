46448c4277af1e7cfa8b7ddd39186ef2252ac931
Samuel Anozie <samuelanozie10@yahoo.com>
2023-03-18 21:57:48 +0100

    Token verification middleware

---
+++ src/middlewares/verifyUser.ts
@@ -0,0 +1,38 @@
+import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import User from '../models/user';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const jwtSecret: Secret = process.env.JWT_SECRET as string;
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.header('Authorization')?.replace('Bearer ', '') || '';
+
+        if (!token)
+            return res.status(401).json({
+                error: 'Invalid Authentication',
+            });
+
+        const decoded = jwt.verify(token as string, jwtSecret);
+        const { id } = decoded as JwtPayload;
+
+        const findUser = await User.findOne({ _id: id });
+        if (!findUser)
+            return res.status(401).json({
+                error: 'Invalid Authentication',
+            });
+
+        req.params.logged_user_id = findUser.id;
+        req.params.logged_user_name = findUser.name;
+        req.params.logged_email = findUser.email;
+    } catch (err) {
+        return res.status(401).json({
+            error: (err as Error).message,
+        });
+    }
+    next();
+};
