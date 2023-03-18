d76a1e41fb460866e40a930865a460aa7e3d5f0d
Samuel Anozie <samuelanozie10@yahoo.com>
2023-03-18 21:56:05 +0100

    Utilities

---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt';
+const iplocate = require('node-iplocate');
+
+export const getIpLocation = async (ipAddress: string) => {
+    try {
+        const location = await iplocate(ipAddress);
+        return location;
+    } catch (err) {
+        console.log(err);
+        return {};
+    }
+};
+
+export const hashPassword = async (password: string) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
+
+export const comparePassword = async (
+    password: string,
+    receivedPassword: string
+) => {
+    return await bcrypt.compare(password, receivedPassword);
+};
