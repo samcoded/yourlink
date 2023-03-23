767f6dae8955477de019d980470cf5837fe90b25
Samuel Anozie <samuelanozie10@yahoo.com>
2023-03-24 00:56:48 +0100

    fix clipboard copy

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -139,13 +139,48 @@ function Main() {
         return result.trim();
     }
 
-    const copyClipboard = (data) => {
-        navigator.clipboard.writeText(data).then(function () {
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            // var msg = successful ? 'successful' : 'unsuccessful';
+            // console.log('Fallback: Copying text command was ' + msg);
+            alert("Copied to clipboard!")
+        } catch (err) {
+            // console.error('Fallback: Oops, unable to copy', err);
+        }
+
+        document.body.removeChild(textArea);
+    }
+    function copyClipboard(text) {
+        if (!navigator.clipboard) {
+            fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(function () {
             alert("Copied to clipboard")
-        }, function () {
-            alert("Failed to copy to clipboard")
+        }, function (err) {
+            // console.error('Async: Could not copy text: ', err);
         });
     }
+    // const copyClipboard = (data) => {
+    //     navigator.clipboard.writeText(data).then(function () {
+    //         alert("Copied to clipboard")
+    //     }, function () {
+    //         alert("Failed to copy to clipboard")
+    //     });
+    // }
 
 
     return (
